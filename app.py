59144199fafb8448761340b15484a3213314bafc
Pandey <neetu.pandey@capgemini.com>
2025-01-09 09:43:13 +0530

    add missing line appfile

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    # Ensure it's listening on 0.0.0.0 for Docker to access it
+    app.run(host='0.0.0.0', port=5000)
